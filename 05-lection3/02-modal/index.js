22b0ed705ccc180b3267f6d0625933800cf205ba
Andrey <babadook777@gmail.com>
2023-11-29 21:40:33 +0500

    add btn and fix modal style

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,4 +1,5 @@
 (function() {
+    console.log('start');
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
@@ -8,6 +9,7 @@
     }
 
     modalOpen.addEventListener('click', () => {
+        console.log('test');
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
     });
